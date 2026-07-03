89a5f097011b1dcb74def67cdafc0d6bc39220da
Olivia Kusuma <okus245@aucklanduni.ac.nz>
2026-07-03 23:16:11 +1200

    fix font extraction error and font color

--- src/main/java/com/kirbken/Main.java
+++ src/main/java/com/kirbken/Main.java
@@ -1,12 +1,16 @@
 package com.kirbken;
 
 import javafx.application.Application;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        System.out.println(Font.loadFont(
+            getClass().getResourceAsStream("/fonts/GeistPixelRegular.ttf"), 20
+        ).getName());
         SceneManager manager = new SceneManager(primaryStage);
         primaryStage.setTitle("Puff Daddy");
         manager.goToStart();
