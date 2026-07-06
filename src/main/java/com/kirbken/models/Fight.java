2e6ae0b7091d678b41dbe2fa7180a6f0dd58a4dc
Alyza So <aso060@aucklanduni.ac.nz>
2026-07-07 00:32:44 +1200

    create a fight.java to implement the fighting logic under models

---
+++ src/main/java/com/kirbken/models/Fight.java
@@ -0,0 +1,45 @@
+package com.kirbken.models;
+
+public class Fight {
+
+    private static final double ATTACK_RANGE = 120;
+    private static final int ATTACK_DAMAGE = 1;
+
+    private final Character p1;
+    private final Character p2;
+    private boolean isOver = false;
+    private Character winner = null;
+
+    public Fight(Character p1, Character p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public void update() {
+        if (isOver) return;
+        checkCollisions();
+        checkWinCondition();
+    }
+
+    private void checkCollisions() {
+        double distance = Math.abs(p1.getX() - p2.getX());
+
+        if (distance < ATTACK_RANGE) {
+            if (p1.isAttacking()) p2.takeDamage(ATTACK_DAMAGE);
+            if (p2.isAttacking()) p1.takeDamage(ATTACK_DAMAGE);
+        }
+    }
+
+    private void checkWinCondition() {
+        if (p1.getHealth() <= 0) {
+            isOver = true;
+            winner = p2;
+        } else if (p2.getHealth() <= 0) {
+            isOver = true;
+            winner = p1;
+        }
+    }
+
+    public boolean isOver() { return isOver; }
+    public Character getWinner() { return winner; }
+}
