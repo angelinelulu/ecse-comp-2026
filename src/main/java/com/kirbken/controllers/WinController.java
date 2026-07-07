5e7de28dda9525f2058884f902ecc52b894658d3
angeline <alul971@aucklanduni.ac.nz>
2026-07-08 07:46:17 +1200

    added buttons and configured css style

--- src/main/java/com/kirbken/controllers/WinController.java
+++ src/main/java/com/kirbken/controllers/WinController.java
@@ -1,9 +1,16 @@
 package com.kirbken.controllers;
 
 import com.kirbken.SceneManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class WinController implements FxController {
     private SceneManager manager;
+
+    @FXML private Label lblWinLose;
+    @FXML private Button btnPlayAgain;
+    @FXML private Button btnHome;
 
     @Override
     public void setSceneManager(SceneManager manager) {
