9d129be9684032b087a3f71ef0cec709c8a65320
angeline <alul971@aucklanduni.ac.nz>
2026-07-08 07:52:05 +1200

    configured the buttons to return to home or play again

--- src/main/java/com/kirbken/controllers/WinController.java
+++ src/main/java/com/kirbken/controllers/WinController.java
@@ -1,6 +1,8 @@
 package com.kirbken.controllers;
 
 import com.kirbken.SceneManager;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -18,4 +20,16 @@ public class WinController implements FxController {
     }
 
     // Add @FXML fields/methods once you know what buttons win.fxml has
+
+    // if player clicks play again, go to story
+    @FXML
+    public void onPlayAgain(ActionEvent event) {
+        manager.goToStory();
+    }
+
+    // if player clicks home, go to start
+    @FXML
+    public void onHome(ActionEvent event) {
+        manager.goToStart();
+    }
 }
