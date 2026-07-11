4f6b8d94a7336227667a7a0f14326771557ef2e0
angeline <alul971@aucklanduni.ac.nz>
2026-07-11 22:53:01 +1200

    added elements needed for the lose screen

--- src/main/java/com/kirbken/controllers/LoseController.java
+++ src/main/java/com/kirbken/controllers/LoseController.java
@@ -2,6 +2,9 @@ package com.kirbken.controllers;
 
 import com.kirbken.SceneManager;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class LoseController implements FxController {
     private SceneManager manager;
 
@@ -11,4 +14,16 @@ public class LoseController implements FxController {
     }
 
     // Add @FXML fields/methods once you know what buttons lose.fxml has
+
+    @FXML
+    public void onPlayAgain(ActionEvent event) {
+        // Play again should return to the arena to rematch
+        manager.goToArena();
+    }
+
+    // if player clicks home, go to start
+    @FXML
+    public void onHome(ActionEvent event) {
+        manager.goToStart();
+    }
 }
