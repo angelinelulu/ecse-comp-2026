574a707fa9f7b86437cb45979500a4214176ef2a
angeline <alul971@aucklanduni.ac.nz>
2026-07-22 13:21:04 +1200

    configured the buttons to return to start and go back to settings

--- src/main/java/com/kirbken/controllers/QuitController.java
+++ src/main/java/com/kirbken/controllers/QuitController.java
@@ -1,21 +1,30 @@
 package com.kirbken.controllers;
 
+import com.kirbken.SceneManager;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class QuitController {
+public class QuitController implements FxController {
     @FXML private Button btnYes;
     @FXML private Button btnNo;
     @FXML private Label lblConfirmation;
 
+    private SceneManager manager;
+
+    @Override
+    public void setSceneManager(SceneManager manager) {
+        this.manager = manager;
+    }
+
     @FXML
     private void onYesClicked() {
-        // Exit the application
+        manager.goToStart(); // Navigate to the start screen before quitting
     }
 
     @FXML
     private void onNoClicked() {
-        // Return to SettingsController without quitting the application
+        manager.goToSettings();
     }
 }
